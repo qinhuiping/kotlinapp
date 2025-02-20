e81df3ac0fcc56fe2f60fee91242c057b645063d
huiping.qin <huiping.qin@zkteco.com>
2025-02-20 11:07:54 +0800

    权限申请失败手动开启，和回到顶部

--- app/src/main/java/com/qhping/kotlin/app/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/qhping/kotlin/app/ui/gallery/GalleryFragment.kt
@@ -1,14 +1,18 @@
 package com.qhping.kotlin.app.ui.gallery
 
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -90,24 +94,41 @@ class GalleryFragment : Fragment() {
                 imageAdapter.setList(imageList)
             }
         }
+
+        binding.textGallery.setOnClickListener {
+            binding.rvImage.scrollToPosition(0)
+            binding.rvImage.smoothScrollToPosition(0)
+        }
     }
 
 
     private val requestMultiplePermission =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permission ->
-            permission.entries.forEach { it ->
+            permission.entries.forEach {
                 val pName = it.key
                 val isGranted = it.value
+                var shouldShowRationale = false
+                var allPermissionsGranted = true
                 if (isGranted) {
                     Toast.makeText(requireContext(), "$pName 权限已授予", Toast.LENGTH_SHORT).show()
 
                 } else {
                     Toast.makeText(requireContext(), "$pName 被拒绝", Toast.LENGTH_SHORT).show()
+                    allPermissionsGranted = false
+                    if (shouldShowRequestPermissionRationale(pName)) {
+                        shouldShowRationale = true
+                    }
                 }
 
-                val allPermissionsGranted = permission.values.all { it }
+//                allPermissionsGranted = permission.values.all { it }
                 if (allPermissionsGranted) {
                     galleryViewModel.loadGalleryImages()
+                } else {
+                    if (shouldShowRationale) {
+                        showPermissionRationale(permission.keys.toTypedArray())
+                    } else {
+                        showSettingDialog()
+                    }
                 }
             }
         }
@@ -115,6 +136,27 @@ class GalleryFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+    }
+
+    private fun showPermissionRationale(permissions: Array<String>) {
+        AlertDialog.Builder(requireContext()).setTitle("权限请求")
+            .setMessage("应用需要这些权限才能正常工作，请授权").setPositiveButton("确定") { _, _ ->
+                requestMultiplePermission.launch(permissions)
+            }.setNegativeButton("取消") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    private fun showSettingDialog() {
+        AlertDialog.Builder(requireContext()).setTitle("权限被拒绝")
+            .setMessage("你已经永久拒绝了某些权限，请在应用设置中手动开启")
+            .setPositiveButton("去设置") { _, _ ->
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                val uri = Uri.fromParts("package", requireContext().packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }.setNegativeButton("取消") { dialog, _ -> dialog.dismiss() }.show()
     }
 
 
